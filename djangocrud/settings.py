bb547af096c52b963174ed65036aeee192be6d4c
justinz12xd <justinalejandro996@gmail.com>
2024-08-29 23:32:32 -0500

    Update settings.py

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -151,8 +151,7 @@ SECRET_KEY = 'dffggfgfgf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['prueba2.pythonanywhere.com']
 
 # Application definition
 
